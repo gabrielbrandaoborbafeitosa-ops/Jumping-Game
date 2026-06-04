6adf38d4b87209cb89865b396fc245bbb713abe8
gabrielbrandaoborbafeitosa-ops <gabrielbrandaoborbafeitosa@gmail.com>
2026-06-04 18:07:33 -0300

    novo JS

--- script.js
+++ script.js
@@ -94,4 +94,6 @@ if (colide(matar4, player)) {
  const celular = /Android|iPhone|iPad|iPod|Mobile/i.test(navigator.userAgent);
  if (celular) {
     alert('Erro\nEsse site não está disponível para:\nAndroid, Iphone, Ipad, Ipod e Mobiles.\nTente abrir esse site no computador.')
+ } else {
+    alert('Bem vindo!')
  }
